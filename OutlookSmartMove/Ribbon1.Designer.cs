76b4be6e00a1297956c38962b0b3fde3916181d9
Thomas Vuylsteke <thovuy@microsoft.com>
2018-10-27 17:27:10 +0000

    initial version

---
+++ OutlookSmartMove/Ribbon1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace OutlookSmartMove
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.moveButton = this.Factory.CreateRibbonButton();
+            this.detectButton = this.Factory.CreateRibbonButton();
+            this.destinationLabel = this.Factory.CreateRibbonLabel();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.ControlId.OfficeId = "TabMail";
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabMail";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.moveButton);
+            this.group1.Items.Add(this.detectButton);
+            this.group1.Items.Add(this.destinationLabel);
+            this.group1.Label = "Smart Move";
+            this.group1.Name = "group1";
+            // 
+            // moveButton
+            // 
+            this.moveButton.Label = "Move";
+            this.moveButton.Name = "moveButton";
+            // 
+            // detectButton
+            // 
+            this.detectButton.Label = "Detect";
+            this.detectButton.Name = "detectButton";
+            this.detectButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.detectButton_Click);
+            // 
+            // destinationLabel
+            // 
+            this.destinationLabel.Label = ".";
+            this.destinationLabel.Name = "destinationLabel";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Outlook.Explorer";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton moveButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton detectButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel destinationLabel;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ OutlookSmartMove/Ribbon1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace OutlookSmartMove
+{
+    public partial class Ribbon1
+    {
+
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void detectButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            Outlook.Explorer explorer = Globals.ThisAddIn.Application.ActiveExplorer();
+            Outlook.MAPIFolder inBox = (Outlook.MAPIFolder)Globals.ThisAddIn.Application.ActiveExplorer().Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+                        
+            Outlook.MAPIFolder temp = inBox.Folders["FTA"];
+            Outlook.MAPIFolder destFolder = temp.Folders["_Test"];
+
+            if (explorer != null && explorer.Selection != null && explorer.Selection.Count > 0)
+            {
+                object item = explorer.Selection[1];
+                if (item is Outlook.MailItem)
+                {
+                    Outlook.MailItem mailItem = item as Outlook.MailItem;
+                    mailItem.Move(destFolder);
+                }
+            }
+        }
+    }
+}
